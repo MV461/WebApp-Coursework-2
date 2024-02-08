e27dc6d25ff059f424f67aede4a05fef429c717f
Milan Varughese <milanvarughese@outlook.com>
2024-02-08 15:48:16 +0000

    Added functionality to index.js to connect to MongoDB, and get lessons from it. Also added a route to get all lessons.
    Also added mongodb package to package.json .

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const path = require('path');
+
+const uri = "mongodb+srv://mv461:uZDFRgnuQ297HPPi@myfirstcluster.5zcjeqz.mongodb.net/";
+const client = new MongoClient(uri);
+let db, lessonsCollection, ordersCollection;
+
+client.connect()
+  .then(() => {
+    console.log("Connected to MongoDB!")
+
+    db = client.db("webapp-cw2")
+    lessonsCollection = db.collection('lessons');
+    ordersCollection = db.collection('orders');
+
+
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB or performing operation", err);
+  })
+
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public'), { index: 'index.html' }));
 
+app.get("/lessons", (request, response) => {
+  lessonsCollection.find({}).toArray()
+    .then((documents) => {
+      response.json(documents)
+    })
+    .catch((err) => {
+      response.status.send(err);
+    })
+})
+
 // Set up a listener for your server
-const PORT = process.env.PORT ||  3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
